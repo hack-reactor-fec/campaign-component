dfedbb717cae48960a27578f9450f7cd0def8165
r-hall <ryanhall2015@gmail.com>
2018-04-18 19:28:01 -0700

    brought estimated delivery and ships inline

--- client/src/components/SingleCurrentLevel.jsx
+++ client/src/components/SingleCurrentLevel.jsx
@@ -16,13 +16,15 @@ function SingleCurrentLevel(props) {
 						{props.level.includes.map(item => <li className="includes-list-item">{item}</li>)}
 					</ul>
 				</div>
-				<div className="level-delivery-date-section">
-					<h4 className="level-additional-info-header">ESTIMATED DELIVERY</h4>
-					<div className="level-additional-info-item">{moment(props.level.estimatedDelivery).format('MMM YYYY')}</div>
-				</div>
-				<div className="level-delivery-region-section">
-					<h4 className="level-additional-info-header">SHIPS TO</h4>
-					<div className="level-additional-info-item">{props.level.shipsTo}</div>
+				<div className="level-extra-info-section">
+					<div className="level-delivery-date-section">
+						<span className="level-additional-info-header">ESTIMATED DELIVERY</span>
+						<span className="level-additional-info-item">{moment(props.level.estimatedDelivery).format('MMM YYYY')}</span>
+					</div>
+					<div className="level-delivery-region-section">
+						<span className="level-additional-info-header">SHIPS TO</span>
+						<span className="level-additional-info-item">{props.level.shipsTo}</span>
+					</div>
 				</div>
 				<LevelBackersSection level={props.level} levelType={props.levelType}/>
 			</div>
